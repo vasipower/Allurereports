ec2e607750ffef87eb69466775becae4811ff569
vasista.tvn <vasista.tvn@rakuten.com>
2021-02-08 23:47:39 +0530

    reports

---
+++ src/main/java/sample/Test1.java
@@ -0,0 +1,34 @@
+package sample;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
+
+public class Test1 {
+
+	@Story("This is a Fail Story.")
+	@Description("This is a Fail Story Description.")
+	@Test
+	void m1() {
+
+		System.out.println("Welcome to allure");
+
+	}
+
+	@Test
+	@Story("This is a Fail Story.")
+	@Description("This is a Fail Story Description.")
+	void m11() {
+		int a = 1;
+
+		Assert.assertEquals(a, 11, "Your debug message that you want to display in case of failure");
+
+//	if(a==11) {
+//	System.out.println("Welcome to allure-1233");
+//	
+//	}
+	}
+
+}
